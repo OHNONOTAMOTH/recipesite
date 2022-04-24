432ebffbf2857f24e7a9674dae6b691d1c193999
moth <moth>
2022-04-24 10:34:23 -0500

    fixed search [trans rights]

--- src/search.rs
+++ src/search.rs
@@ -1,10 +1,10 @@
 use redis::*;
 
-pub fn search(title: &str, mut con: Connection) -> Result<String, RedisError> {
+pub fn search(title: &str, mut con: Connection) -> Result<Vec<String>, RedisError> {
     let result = redis::cmd("ft.sugget")
         .arg("recipesearch")
         .arg(title)
         .query(&mut con);
 
-    Ok(result)
+    Ok(result.unwrap())
 }
