8d05886e11658c6a6236c95b7a4389bb999384b0
moth <moth>
2022-04-25 08:39:34 -0500

    added comments [trans rights]

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,25 @@ const ADDR: &str = "redis://127.0.0.1";
 
 #[launch]
 async fn rocket() -> _ {
-    //submit::submittosearch();
-    
-    //println!("{:?}", search::search("test", con).await.unwrap());
-    rocket::build().mount("/", routes![route])
+  //submit::submittosearch();
+  
+  //println!("{:?}", search::search("test", con).await.unwrap());\
+  
+  //  start server
+  rocket::build().mount("/", routes![route])
 }
 
+//  rest api for searching db
 #[get("/search/<url>")]
 async fn route(url: &str) -> Json<String> {
+  //  creates connection
   let client = redis::Client::open(ADDR).unwrap();
   let con = client.get_connection().unwrap();
-
+  
+  //  searches db for input string
   let results = search::search(url, con).await;
+  
+  //  returns json of results
   response::content::Json(json::stringify(object! {
     results: results.unwrap()
   }))
