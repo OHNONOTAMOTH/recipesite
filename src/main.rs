54a2e05a1aebbbbfe722e8a3098d9668c31859d5
moth <moth>
2022-05-05 13:50:51 -0500

    fixed CORS [trans rights]

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,45 @@ use json::*;
 use rocket::*;
 use rocket::response::content::{Json, Custom};
 use rocket::http::ContentType;
+use rocket_cors::{AllowedOrigins, CorsOptions};
 
 pub mod search;
 pub mod submit;
 
 const ADDR: &str = "redis://127.0.0.1";
 
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+//  added CORS
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 async fn rocket() -> _ {
   //submit::submittosearch();
   
   //println!("{:?}", search::search("test", con).await.unwrap());\
-  
+
   //  start server
-  rocket::build().mount("/", routes![servesearchres, submittorest])
+  rocket::build().attach(CORS).mount("/", routes![servesearchres, submittorest])
 }
 
 //  rest api for searching db
